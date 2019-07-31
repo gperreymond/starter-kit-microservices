02b7336124589b87813b41a6a80edfe8cbc40ab6
Gilles Perreymond <gilles@lalalab.com>
2019-07-31 06:51:53 +0200

    feat(core): finish first service/route

--- domains/Books/GoogleBooksApiPerformSearchByISBNQuery/service.js
+++ domains/Books/GoogleBooksApiPerformSearchByISBNQuery/service.js
@@ -6,9 +6,10 @@ const { params } = require('./validate')
 const handler = async (ctx) => {
   try {
     const { isbn } = ctx.params
-    const URL = `${Configuration.google.api.books.url}?q=${isbn}&key=${Configuration.google.api.books.key}`
+    const URL = `${Configuration.google.api.books.url}?q=isbn:${isbn}&key=${Configuration.google.api.books.key}`
     const { data } = await axios.get(URL)
-    return data
+    if (data.totalItems === 0) { throw new Error('Book not found') }
+    return data.items[0]
   } catch (e) { return Promise.reject(e) }
 }
 
